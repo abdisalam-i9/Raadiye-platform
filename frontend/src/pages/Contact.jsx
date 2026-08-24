a0d676edcfd39204e4d72e1ae1086dd67464bf1e
shkuur <shkuur1234@gmail.com>
2026-08-24 11:06:35 -0700

    waxa aan sameeyay Contact Page  ka

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,18 @@
-import Page from '../components/Page';
 
-function Contact() {
-  return <Page title="Contact" />;
-}
+import Header from "../components/Header";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+
+      <main className="mx-auto max-w-[1200px] px-5 py-16">
+        <h1 className="text-4xl font-extrabold text-indigo-950">
+          Contact LostAndFound Team
+        </h1>
+      </main>
+    </>
+  );
+};
 
 export default Contact;
