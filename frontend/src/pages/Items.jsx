d67690cafcce56bf1ff70fcea3219f81c19abefc
abaskadev <abaskadev@gmail.com>
2026-08-24 11:19:58 +0300

    Items Page ayaan kusoo daray

---
+++ frontend/src/pages/Items.jsx
@@ -0,0 +1,14 @@
+import BrowseSection from "../components/Home/BrowseSection";
+
+const Items = () => {
+  return (
+    <>
+
+      <main className="py-10">
+        <BrowseSection />
+      </main>
+    </>
+  );
+};
+
+export default Items;
